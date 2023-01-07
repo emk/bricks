0540bc98e72a5ecd06a34de0aa443dc8b611fca2
Eric Kidd <git@randomhacks.net>
2023-01-07 07:08:44 -0500

    Add basic collisions and sound

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, time::FixedTimestep};
 
 // 1 physics tick.
 const TIME_STEP: f32 = 1.0 / 60.0;
@@ -11,7 +11,13 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup)
-        .add_system(apply_velocity)
+        .add_event::<CollisionEvent>()
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
+                .with_system(apply_velocity)
+                .with_system(play_collision_sound.after(apply_velocity)),
+        )
         .add_system(bevy::window::close_on_esc)
         .run();
 }
@@ -28,11 +34,17 @@ struct ScreenBounds {
     top_right: Vec3,
 }
 
+#[derive(Resource)]
+struct CollisionSound(Handle<AudioSource>);
+
+#[derive(Default)]
+struct CollisionEvent;
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    //asset_server: Res<AssetServer>,
+    asset_server: Res<AssetServer>,
     windows: ResMut<Windows>,
 ) {
     // Set up our camera.
@@ -53,10 +65,12 @@ fn setup(
     println!("bounds: {:?}", bounds);
     commands.insert_resource(bounds);
 
+    commands.insert_resource(CollisionSound(asset_server.load("click.ogg")));
+
     // Ball
     let ball_pos = Vec3::new(0.0, 0.0, 1.0);
     let ball_size = Vec3::new(30.0, 30.0, 30.0);
-    let ball_speed = 100.0;
+    let ball_speed = 1000.0;
     commands.spawn((
         MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::default().into()).into(),
@@ -72,22 +86,39 @@ fn setup(
 fn apply_velocity(
     bounds: Res<ScreenBounds>,
     mut query: Query<(&mut Transform, &mut Velocity)>,
+    mut collision_events: EventWriter<CollisionEvent>,
 ) {
     for (mut transform, mut velocity) in &mut query {
         if transform.translation.x < bounds.bottom_left.x {
+            collision_events.send_default();
             velocity.x = -velocity.x;
         }
         if transform.translation.x >= bounds.top_right.x {
+            collision_events.send_default();
             velocity.x = -velocity.x;
         }
         if transform.translation.y < bounds.bottom_left.y {
+            collision_events.send_default();
             velocity.y = -velocity.y;
         }
         if transform.translation.y >= bounds.top_right.y {
+            collision_events.send_default();
             velocity.y = -velocity.y;
         }
 
         transform.translation.x += velocity.x * TIME_STEP;
         transform.translation.y += velocity.y * TIME_STEP;
+    }
+}
+
+fn play_collision_sound(
+    collision_events: EventReader<CollisionEvent>,
+    audio: Res<Audio>,
+    sound: Res<CollisionSound>,
+) {
+    // Only play a single sound, and clear the event for the next frame.
+    if !collision_events.is_empty() {
+        collision_events.clear();
+        audio.play(sound.0.clone());
     }
 }
